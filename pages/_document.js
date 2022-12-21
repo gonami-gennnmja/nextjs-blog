00c55c8a5f044ed0e89e03c1efcebd0d8bcfd921
gonami-gennnnmja <gain1113@icloud.com>
2022-12-21 16:39:20 +0900

    add favicon(snowman☃️)

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
-        <Head />
+        <Head>
+            <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>☃️</text></svg>"></link>
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
